bad5f4cf87083b92cce3bf0f1f4b20f0943ab969
wwwcomw1239-tech <wwwcomw1239-tech@users.noreply.github.com>
2026-03-16 12:54:50 +0000

    deploy: e307a05afdb6451a7ed174623b180547fbcb96a9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "584710625"
+    serviceWorkerVersion: "3867663569"
   }
 });
