bf80f73b673cf04cd7f7bedcc84ae8e1bade8bcf
wwwcomw1239-tech <wwwcomw1239-tech@users.noreply.github.com>
2026-03-16 11:06:50 +0000

    deploy: dbfc3a9358962ff1bacead5bc14faafc804f4dd7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93
 "index.html": "243a3ae903b288a952b4a80abf6c812f",
 "/": "243a3ae903b288a952b4a80abf6c812f",
 "manifest.json": "bf66c0ecfa51a762b315e2339e16329b",
-"flutter_bootstrap.js": "1318715110fad870272f12d9514654b6",
+"flutter_bootstrap.js": "65d636c94b2a2a7825c2f09d245f0301",
 "main.dart.js": "232ae049490c21b2b701ae6491d27b8c",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
